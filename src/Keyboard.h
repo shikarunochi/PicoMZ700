28a2752d1433f745c5486f592945a2489321614a
Prime9999 <Prime9999@users.noreply.github.com>
2025-12-05 17:43:50 +0900

    Modified i2c_kbd speed for stability

--- src/Keyboard.h
+++ src/Keyboard.h
@@ -30,7 +30,7 @@ public:
 #define I2C_KBD_SDA 6
 #define I2C_KBD_SCL 7
 
-#define I2C_KBD_SPEED  400000 // if dual i2c, then the speed of keyboard i2c should be 10khz
+#define I2C_KBD_SPEED  50000 // if dual i2c, then the speed of keyboard i2c should be 10khz
 
 #define I2C_KBD_ADDR 0x1F
 
